1166f3cec8402094abc8124fdd90074d6433934d
Brett Green <brettalanagreen@gmail.com>
2018-04-13 22:28:42 -0500

    untrack bloatware 2nd migration file

--- tigerlilly_site/migrations/0003_auto_20180326_2213.py
+++ tigerlilly_site/migrations/0003_auto_20180326_2213.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tigerlilly_site', '0002_auto_20180114_2152'),
+        ('tigerlilly_site', '0001_initial'),
     ]
 
     operations = [
